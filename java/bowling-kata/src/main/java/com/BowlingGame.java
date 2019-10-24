00a9b5f25a7c85f10104c152f541ca4f9f301525
KristofferHaakonsen <kristoffer@hakonsen.com>
2019-10-24 23:36:19 +0200

    #1 Create tests and remove testcases from main

--- java/bowling-kata/src/main/java/com/BowlingGame.java
+++ java/bowling-kata/src/main/java/com/BowlingGame.java
@@ -1,30 +1,11 @@
 package com;
 
 /**
- * The main class that is used for testing the BowlingScorer during development
+ * The main class
  */
 public class BowlingGame {
 
     public static void main(String[] args){
-        //Different rolls which I should check to confirm the functionality of BowlingScorer
-        int[] zeroRolls = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
-        int[] singleOneRolls = {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
-        int[] oneRolls = {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
-        int[] oneSpareRollsZeroes = {5,5,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
-        int[] oneSpareRollsTwoOnes = {5,5,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
-        int[] oneStrikeRollsZeroes = {10,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
-        int[] oneStrikeRollsTwoOnes = {10,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
 
-        //Create a new BowlingScorerer
-        BowlingScorer scorer = new BowlingScorer();
-
-        //Test and print the different results
-        System.out.println("Score for zero's: " + scorer.calculateScore(zeroRolls));
-        System.out.println("Score for single one: " + scorer.calculateScore(singleOneRolls));
-        System.out.println("Score for one's: " + scorer.calculateScore(oneRolls));
-        System.out.println("Score for one spare and zeroes: " + scorer.calculateScore(oneSpareRollsZeroes));
-        System.out.println("Score for one spare and two one's: " + scorer.calculateScore(oneSpareRollsTwoOnes));
-        System.out.println("Score for one strike and zeroes: " + scorer.calculateScore(oneStrikeRollsZeroes));
-        System.out.println("Score for one strike and two one's: " + scorer.calculateScore(oneStrikeRollsTwoOnes));
     }
 }
--- java/bowling-kata/src/test/java/com/BowlingScorerTest.java
+++ java/bowling-kata/src/test/java/com/BowlingScorerTest.java
@@ -1,8 +1,50 @@
 package com;
 
-/**
- * This class will test BowlingScorer
- * */
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
+/**
+ * This class tests the BowlingScorer's methods
+ * */
 public class BowlingScorerTest {
+    private BowlingScorer scorer = new BowlingScorer();
+
+    //Test rolls
+    private int[] zeroRolls = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+    private int[] oneRolls = {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
+    private int[] nineAndZeroRolls = {9,0,9,0,9,0,9,0,9,0,9,0,9,0,9,0,9,0,9,0};
+    private int[] spareRolls = {5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5};
+    private int[] strikeRolls = {10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10};
+
+    //Tests calculateScore with only zero rolls
+    @Test
+     public void calculateOnlyZeroesScore() {
+        assertEquals(0, scorer.calculateScore(zeroRolls) );
+    }
+
+    //Tests calculateScore with only one rolls
+    @Test
+    public void  calculateOnlyOnesScore() {
+        assertEquals(20, scorer.calculateScore(oneRolls) );
+    }
+
+    //Tests calculateScore with every other roll being nine and zero
+    @Test
+    public void  calculateNinesAndZeroesRollsScore() {
+        assertEquals(90, scorer.calculateScore(nineAndZeroRolls) );
+    }
+
+    //Tests calculateScore with every roll being a spare
+    @Test
+    public void  calculateOnlySparesScore() {
+        assertEquals(150, scorer.calculateScore(spareRolls) );
+    }
+
+    //Tests calculateScore with every roll being a strike
+    @Test
+    public void  calculateOnlyStrikesScore() {
+        assertEquals(300, scorer.calculateScore(strikeRolls) );
+    }
+
+
 }
