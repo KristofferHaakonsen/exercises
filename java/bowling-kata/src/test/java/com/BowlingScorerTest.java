5ff6465d1e4cfcde83d76f686dd0447816703b7a
KristofferHaakonsen <kristoffer@hakonsen.com>
2019-10-25 09:47:51 +0200

    #1 Test both BowlingScorer and BowlingGame

--- java/bowling-kata/src/test/java/com/BowlingScorerTest.java
+++ java/bowling-kata/src/test/java/com/BowlingScorerTest.java
@@ -1,6 +1,5 @@
 package com;
 
-import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
@@ -8,7 +7,7 @@ import static org.junit.Assert.assertEquals;
  * This class tests the BowlingScorer's methods
  * */
 public class BowlingScorerTest {
-    private BowlingScorer scorer;
+    private BowlingScorer scorer = new BowlingScorer();
 
     //Test rolls
     private int[] zeroRolls = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
@@ -17,57 +16,34 @@ public class BowlingScorerTest {
     private int[] spareRolls = {5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5};
     private int[] strikeRolls = {10,10,10,10,10,10,10,10,10,10,10,10};
 
-    @Before
-    public void setUp(){
-        scorer = new BowlingScorer();
-    }
-
     //Tests calculateScore with only zero rolls
     @Test
      public void calculateOnlyZeroesScore() {
-        for(int i : zeroRolls){
-            scorer.rollOnce(i);
-        }
-
-        assertEquals(0, scorer.calculateScore() );
+        assertEquals(0, scorer.calculateScore(zeroRolls) );
     }
 
     //Tests calculateScore with only one rolls
     @Test
     public void  calculateOnlyOnesScore() {
-        for(int i : oneRolls){
-            scorer.rollOnce(i);
-        }
-
-        assertEquals(20, scorer.calculateScore() );
+        assertEquals(20, scorer.calculateScore(oneRolls) );
     }
 
     //Tests calculateScore with every other roll being nine and zero
     @Test
     public void  calculateNinesAndZeroesRollsScore() {
-        for(int i : nineAndZeroRolls){
-            scorer.rollOnce(i);
-        }
-        assertEquals(90, scorer.calculateScore() );
+        assertEquals(90, scorer.calculateScore(nineAndZeroRolls) );
     }
 
     //Tests calculateScore with every roll being a spare
     @Test
     public void  calculateOnlySparesScore() {
-        for(int i : spareRolls){
-            scorer.rollOnce(i);
-        }
-        assertEquals(150, scorer.calculateScore() );
+        assertEquals(150, scorer.calculateScore(spareRolls) );
     }
 
     //Tests calculateScore with every roll being a strike
     @Test
     public void  calculateOnlyStrikesScore() {
-        for(int i : strikeRolls){
-            scorer.rollOnce(i);
-        }
-
-        assertEquals(300, scorer.calculateScore() );
+        assertEquals(300, scorer.calculateScore(strikeRolls) );
     }
 
 
