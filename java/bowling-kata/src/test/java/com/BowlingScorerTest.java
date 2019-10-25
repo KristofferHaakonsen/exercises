a2e42a1828154cc12d78684ff5bc744bb9ae7544
KristofferHaakonsen <kristoffer@hakonsen.com>
2019-10-25 08:34:49 +0200

    #1 Fix error where strikeRolls has 22 rolls instead of 12

--- java/bowling-kata/src/test/java/com/BowlingScorerTest.java
+++ java/bowling-kata/src/test/java/com/BowlingScorerTest.java
@@ -1,5 +1,6 @@
 package com;
 
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
@@ -7,43 +8,66 @@ import static org.junit.Assert.assertEquals;
  * This class tests the BowlingScorer's methods
  * */
 public class BowlingScorerTest {
-    private BowlingScorer scorer = new BowlingScorer();
+    private BowlingScorer scorer;
 
     //Test rolls
     private int[] zeroRolls = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
     private int[] oneRolls = {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
     private int[] nineAndZeroRolls = {9,0,9,0,9,0,9,0,9,0,9,0,9,0,9,0,9,0,9,0};
     private int[] spareRolls = {5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5};
-    private int[] strikeRolls = {10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10};
+    private int[] strikeRolls = {10,10,10,10,10,10,10,10,10,10,10,10};
+
+    @Before
+    public void setUp(){
+        scorer = new BowlingScorer();
+    }
 
     //Tests calculateScore with only zero rolls
     @Test
      public void calculateOnlyZeroesScore() {
-        assertEquals(0, scorer.calculateScore(zeroRolls) );
+        for(int i : zeroRolls){
+            scorer.rollOnce(i);
+        }
+
+        assertEquals(0, scorer.calculateScore() );
     }
 
     //Tests calculateScore with only one rolls
     @Test
     public void  calculateOnlyOnesScore() {
-        assertEquals(20, scorer.calculateScore(oneRolls) );
+        for(int i : oneRolls){
+            scorer.rollOnce(i);
+        }
+
+        assertEquals(20, scorer.calculateScore() );
     }
 
     //Tests calculateScore with every other roll being nine and zero
     @Test
     public void  calculateNinesAndZeroesRollsScore() {
-        assertEquals(90, scorer.calculateScore(nineAndZeroRolls) );
+        for(int i : nineAndZeroRolls){
+            scorer.rollOnce(i);
+        }
+        assertEquals(90, scorer.calculateScore() );
     }
 
     //Tests calculateScore with every roll being a spare
     @Test
     public void  calculateOnlySparesScore() {
-        assertEquals(150, scorer.calculateScore(spareRolls) );
+        for(int i : spareRolls){
+            scorer.rollOnce(i);
+        }
+        assertEquals(150, scorer.calculateScore() );
     }
 
     //Tests calculateScore with every roll being a strike
     @Test
     public void  calculateOnlyStrikesScore() {
-        assertEquals(300, scorer.calculateScore(strikeRolls) );
+        for(int i : strikeRolls){
+            scorer.rollOnce(i);
+        }
+
+        assertEquals(300, scorer.calculateScore() );
     }
 
 
